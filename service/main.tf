e7f3c14ed073885fe38801ab2ad9ed7020e718c6
Cory Kitchens <cjkitche@calpoly.edu>
2019-08-19 11:49:29 -0700

    String interpolation

--- service/main.tf
+++ service/main.tf
@@ -1,16 +1,16 @@
-# Main
-# vars
-# - instance_size
-# - ami_id
+# # Main
+# # vars
+# # - instance_size
+# # - ami_id
 
-# resources
-# - EC2 Server /w Windows 2016
-# - PEM file
+# # resources
+# # - EC2 Server /w Windows 2016
+# # - PEM file
+
 
 data "aws_ssm_parameter" "sg_id" {
   name = "/VSH/SG/ID"
 }
-
 
 resource "tls_private_key" "priv_key" {
   algorithm = "RSA"
@@ -18,14 +18,17 @@ resource "tls_private_key" "priv_key" {
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = var.key_name
+  key_name   = "${var.key_name}"
   public_key = "${tls_private_key.priv_key.public_key_openssh}"
 }
 
 resource "aws_instance" "server" {
-  instance_type          = var.instance_size
-  ami                    = var.ami_id
-  key_name               = aws_key_pair.generated_key.key_name
+  instance_type          = "${var.instance_size}"
+  ami                    = "${var.ami_id}"
+  key_name               = "${var.key_name}"
   get_password_data      = true
   vpc_security_group_ids = ["${data.aws_ssm_parameter.sg_id.value}"]
+  tags = {
+    Name = "VSH-${var.key_name}"
+  }
 }
