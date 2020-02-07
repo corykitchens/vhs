9d37e6d1245d9801c051be8c0fe361fe59e74654
Cory Kitchens <corykitchens@gmail.com>
2020-02-07 08:50:01 -0800

    Updated params

--- service/variables.tf
+++ service/variables.tf
@@ -3,16 +3,16 @@
 # - instance_size
 # - ami_id
 variable "key_name" {
-  type = "string"
+  type    = "string"
   default = "HelloWorld"
 }
 
 variable "instance_size" {
   type    = "string"
-  default = "t2.micro"
+  default = "t3.medium"
 }
 
 variable "ami_id" {
   type    = "string"
-  default = "ami-057549bd0bba43bc1"
+  default = "ami-0a1d405ce719bebfd"
 }
